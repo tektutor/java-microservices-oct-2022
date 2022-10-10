b5f0bf4223e318c697e906cf1ac339255e0dd4ae
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-10-10 06:00:49 -0700

    Added Day1 lab exercises.

---
+++ Day1/reactjs/arrow-functions/src/index.js
@@ -0,0 +1,10 @@
+//const square = function(x) {
+//   return x * x;
+//}
+//
+
+const square = x => x * x;
+const cube = x => x * x * x;
+
+console.log ( '10 x 10 => ' + square(10) )
+console.log ( '5 x 5 x 5 => ' + cube (5) )
