eca54043d636f23d997a79f0ee8dc0342d46b099
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-10-10 02:00:01 -0700

    Added Day1 lab exercises.

---
+++ Day1/javascript/separating-js-from-html-page/myscript.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    console.log ( "sayHello javascript function invoked ..." );
+    document.getElementById('demo').innerText = "Button Clicked - Hello from JavaScript function"
+    console.log ( "sayHello javascript function exit ..." );
+}
+
+function resetToDefaultText() {
+    console.log ( "resetToDefaultText javascript function invoked ..." );
+    document.getElementById('demo').innerText = "Button Clicked - Hello from JavaScript function"
+    console.log ( "resetToDefaultText javascript function exit ..." );
+}
