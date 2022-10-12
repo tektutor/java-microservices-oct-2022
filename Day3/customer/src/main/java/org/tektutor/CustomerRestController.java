dc8ed27c99d60bd8f1ae510ad7eaa4af2cac45cf
Jeganathan Swaminathan <mail2jegan@gmail.com>
2022-10-12 15:01:46 +0530

    Update CustomerRestController.java

--- Day3/customer/src/main/java/org/tektutor/CustomerRestController.java
+++ Day3/customer/src/main/java/org/tektutor/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,8 +21,7 @@ public class CustomerRestController {
 			method=RequestMethod.GET
 	)
 	@ResponseBody	
-	public List<Customer> getAllCustomers() throws Exception {
-		
+	public List<Customer> getAllCustomers() {
 		List<Customer> list = customerRepository.getAllCustomers();
 		return list;
 	}
@@ -32,9 +32,33 @@ public class CustomerRestController {
 	)
 	@ResponseBody
 	public Customer getCustomerById(@PathVariable("customerId")int customerId) 
-		throws Exception
 	{
 		return customerRepository.getCustomerById(customerId);
+	}
+
+	@RequestMapping(
+			value="/api/v1/customer/register", 
+			method=RequestMethod.POST 
+	)
+	
+	public void addCustomer( @RequestBody Customer customer ) {
+		customerRepository.save(customer);
+	}
+	
+	@RequestMapping(
+			value="/api/v1/customer/register", 
+			method=RequestMethod.PATCH 
+	)
+	public void updateCustomer( Customer customer ) {		
+		customerRepository.save(customer);
+	}
+
+	@RequestMapping(
+			value="/api/v1/customer/delete/{customerId}", 
+			method=RequestMethod.DELETE 
+	)
+	public void deleteCustomer(@PathVariable("customerId")int customerId ) {
+		customerRepository.deleteById(customerId);
 	}
 	
 }
